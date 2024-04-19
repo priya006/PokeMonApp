2981b09aae337ae43d299ac19ca0ec9fc3579551
Priya Boopathi <priyanandh06@gmail.com>
2024-04-18 19:42:41 -0700

    CardView Composables added to list page

--- app/src/main/java/com/example/pokemonapp/presentation/composables/PokeMonListComposable.kt
+++ app/src/main/java/com/example/pokemonapp/presentation/composables/PokeMonListComposable.kt
@@ -5,14 +5,17 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
@@ -52,14 +55,20 @@ fun PokeMonListComposable(
 
 @Composable
 fun PokemonListItem(pokemonList: Pokemon,onItemClick: (Int) -> Unit) {
-    Column(
+    Card(
         modifier = Modifier
-            .clickable(onClick = { onItemClick(pokemonList.extractPokemonId() ?: -1)
-            })
-            .padding(16.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .clickable { onItemClick(pokemonList.extractPokemonId() ?: -1) }
     ) {
-        val textValue: String? = pokemonList.name
-        Text(text = textValue ?: "service returned nothing", style = TextStyle(fontSize = 20.sp))
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = pokemonList.name ?: "Service returned nothing",
+                style = TextStyle(fontSize = 20.sp)
+            )
+        }
     }
 }
 
