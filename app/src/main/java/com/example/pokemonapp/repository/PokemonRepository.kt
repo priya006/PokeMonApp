2175a664dbf8af1db4b9498ef462c44c34a06f53
Priya Boopathi <priyanandh06@gmail.com>
2024-04-19 18:21:59 -0700

    kdocs for PokemonRepository

--- app/src/main/java/com/example/pokemonapp/repository/PokemonRepository.kt
+++ app/src/main/java/com/example/pokemonapp/repository/PokemonRepository.kt
@@ -4,12 +4,28 @@ import com.example.pokemonapp.data.model.PokemonDetailsResponse
 import com.example.pokemonapp.data.model.PokemonListResponse
 import com.example.pokemonapp.network.PokeMonApiService
 
+/**
+ * Repository responsible for handling data operations related to Pokémon.
+ *
+ * @property pokeMonApiService The service interface for making API calls related to Pokémon.
+ */
 class PokemonRepository(val pokeMonApiService : PokeMonApiService) {
 
+    /**
+     * Fetches the list of Pokémon from the API.
+     *
+     * @return The list of Pokémon retrieved from the API.
+     */
     suspend fun getPokemonList(): PokemonListResponse {
         return pokeMonApiService.getPokemonList()
     }
 
+    /**
+     * Fetches the details of a specific Pokémon identified by [pokeMonId] from the API.
+     *
+     * @param pokeMonId The ID of the Pokémon whose details are to be fetched.
+     * @return The details of the specified Pokémon retrieved from the API.
+     */
     suspend fun getPokemonDetails(pokeMonId : Int): PokemonDetailsResponse {
         return pokeMonApiService.getPokemonDetails(pokeMonId)
     }
